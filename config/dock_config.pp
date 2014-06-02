cb63e96faf9371ef8c6b28bbe3f1249dda4909ff
wbs75 <wbs75@me.com>
2014-06-02 17:39:02 -0600

    Update dock_config.pp

--- config/dock_config.pp
+++ config/dock_config.pp
@@ -4,9 +4,9 @@ class people::wbs75::config::dock_config (
     $my_username  = $people::wbs75::params::my_username
     ) {
 
-    ##############
+    #################
     # Dock Settings #
-    ##############
+    #################
 
     # Dock prefs only take effect when you restart the dock
     property_list_key { 'Hide the dock':
