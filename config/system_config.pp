f96d73d9a40f135abf5cb13ffa7b53ee9ba0cbc8
wbs75 <wbs75@me.com>
2014-06-02 18:19:30 -0600

    Update system_config.pp

--- config/system_config.pp
+++ config/system_config.pp
@@ -20,7 +20,7 @@ class people::wbs75::config::system_config (
             value   => 'no',
         }
     
-        # Additional information to display on the login window.
+        # Additional information to display on the login window
         property_list_key { 'AdminHostInfo':
             ensure  => present,
             path    => "/Library/Preferences/com.apple.loginwindow.plist",
@@ -39,7 +39,7 @@ class people::wbs75::config::system_config (
             value_type  => 'boolean',
         }
     
-        # Warning: This feature may be unstable. Requires a restart. Enables Quartz 2D extreme functionality, which uses the graphics card to render graphics and text.
+        # Warning: This feature may be unstable. Requires a restart. Enables Quartz 2D extreme functionality, which uses the graphics card to render graphics and text
         property_list_key { 'Enables Quartz 2D':
             ensure  => present,
             path    => "/Library/Preferences/com.apple.windowserver.plist"
@@ -49,7 +49,7 @@ class people::wbs75::config::system_config (
         }
     
         # Disable Automatic Login
-        # Note: change value to "false" for Disable Automatic Login to work.
+        # Note: change value to "false" for Disable Automatic Login to work
         property_list_key { 'Disabl Auto Login Client':
             ensure  => present,
             path    => "/Library/Preferences/com.apple.loginwindow.plist",
@@ -58,7 +58,7 @@ class people::wbs75::config::system_config (
             value_type  => 'boolean',
         }
     
-        # Prevent protection when attempting to remotely control this computer.
+        # Prevent protection when attempting to remotely control this computer
         property_list_key { 'Skip Local Address Check':
             ensure  => present,
             path    => "${my_homedir}/Library/Preferences/com.apple.ScreenSharing.plist",
@@ -67,7 +67,7 @@ class people::wbs75::config::system_config (
             value_type  => 'boolean',
         }
     
-        # Disables system-level key combos like command-option-esc (Force Quit), command-tab (App switcher) to be used on the remote machine.
+        # Disables system-level key combos like command-option-esc (Force Quit), command-tab (App switcher) to be used on the remote machine
         property_list_key { 'Do Not Send SystemKeys':
             ensure  => present,
             path    => "${my_homedir}/Library/Preferences/com.apple.ScreenSharing.plist",
